5fb6f0e3898f81ecea74d9f18096b6c51884478f
Kristen <kr1s10.khud@gmail.com>
2022-02-23 14:01:08 +0200

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    const currSymbol = str[i];
+    let topEl = stack[stack.length - 1];
+
+    for (let p = 0; p < bracketsConfig.length; p++) {
+      const pair = bracketsConfig[p];
+
+      if (currSymbol === pair[0] && currSymbol === pair[1]) {
+
+        if (topEl === currSymbol) stack.pop();
+        else stack.push(currSymbol);
+        
+      } else {
+
+        if (currSymbol === pair[0]) stack.push(currSymbol);
+
+        if (currSymbol === pair[1]) {
+          if (topEl !== pair[0] || stack.pop() === undefined) {
+            return false;
+          }
+        }
+      }
+    }
+  }
+  return stack.length === 0;
 }
